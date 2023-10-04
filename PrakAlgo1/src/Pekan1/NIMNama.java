bdf7e07dafa1d6bce088dd487b05bb656278ba5f
ASUS <ASUS@DESKTOP-MK4DSAL>
2023-10-04 09:02:08 +0700

    Comit pratikum algo pekan 2

---
+++ PrakAlgo1/src/Pekan1/NIMNama.java
@@ -0,0 +1,10 @@
+package Pekan1;
+
+public class NIMNama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2311531006_Nia Ramadhani Tanjung");
+	}
+
+}
---
+++ PrakAlgo1/src/Pekan2/LuasSegitigaw.java
@@ -0,0 +1,21 @@
+package Pekan2;
+
+import java.util.Scanner;
+
+public class LuasSegitigaw {
+
+	public static void main(String[] args) {
+		int alas,tinggi;
+		double luas;
+		Scanner input=new Scanner(System.in);
+		System.out.println("Inputkan Alas= ");
+		alas=input.nextInt ();
+        System.out.println("inputkan tinggi= ");
+        tinggi=input.nextInt();
+        input.close();
+        luas=0.5*alas*tinggi;
+        System.out.println("Luas Segitiga adalah: "	+luas);
+
+	}
+
+}
